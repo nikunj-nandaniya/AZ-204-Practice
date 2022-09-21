6976b0ba9f357c8c856d9819af550846ec3ec36d
nikunjnandaniya <51451918+nikunjnandaniya@users.noreply.github.com>
2022-09-21 20:07:10 +0530

    Added Service Bus Receive Message Solution and restructure folders.

---
+++ ServiceBus/ReceiveMessage/Service Bus-Receive/Service Broker-Receive/Program.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ServiceBus_Queue_Receive
+{
+    class Program
+    {
+        private static string connection_string = "Endpoint=sb://niksservice.servicebus.windows.net/;SharedAccessKeyName=Send;SharedAccessKey=XjRhcW0Z09svEbzsAsB7ECwUGyU+4GfpNQQua3/mg/M=;";
+        private static string queue_name = "niks";
+        static async Task Main(string[] args)
+        {
+            ServiceBusClient _client = new ServiceBusClient(connection_string);
+            ServiceBusReceiver _receiver = _client.CreateReceiver(queue_name, new ServiceBusReceiverOptions() { ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete });
+
+            var _messgaes = _receiver.ReceiveMessagesAsync(2);
+
+            foreach (var _message in _messgaes.Result)
+            {
+                Console.WriteLine($"The Sequence number is {_message.SequenceNumber}");
+                Console.WriteLine(_message.Body);
+            }
+        }
+    }
+}
---
+++ ServiceBus/SendMessage/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.ServiceBus;
+using ServiceBus;
+
+namespace AzureServiceQueue
+{
+    class Program
+    {
+        private static string connection_string = "Endpoint=sb://niksservice.servicebus.windows.net/;SharedAccessKeyName=Send;SharedAccessKey=XjRhcW0Z09svEbzsAsB7ECwUGyU+4GfpNQQua3/mg/M=;";
+        private static string queue_name = "niksservice";
+
+        static void Main(string[] args)
+        {
+            List<Order> _orders = new List<Order>()
+            {
+                new Order() {OrderID="O1",Quantity=10,UnitPrice=9.99m},
+                new Order() {OrderID="O2",Quantity=15,UnitPrice=10.99m },
+                new Order() {OrderID="O3",Quantity=20,UnitPrice=11.99m},
+                new Order() {OrderID="O4",Quantity=25,UnitPrice=12.99m},
+                new Order() {OrderID="O5",Quantity=30,UnitPrice=13.99m }
+            };
+
+            ServiceBusClient _client = new ServiceBusClient(connection_string);
+            ServiceBusSender _sender = _client.CreateSender(queue_name);
+
+            foreach (Order _order in _orders)
+            {
+                ServiceBusMessage _message = new ServiceBusMessage(_order.ToString());
+                _message.ContentType = "application/json";
+                _sender.SendMessageAsync(_message).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
