0762aacb83b87196005dfa342940772aa93a7011
Nikunj Nandaniya <nikunjit007az204@outlook.com>
2022-09-23 20:56:43 +0530

    Topic, Subcription and Filter based on Custom Message Property commit.

---
+++ ServiceBus/ReceiveMessage/Service Bus-Receive/Service Bus-Receive/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ServiceBus_Queue_Receive
+{
+    class Program
+    {
+        private static string connection_string = "Endpoint=sb://niksservice.servicebus.windows.net/;SharedAccessKeyName=Receive;SharedAccessKey=hdnCRr+p22GPtH5QAmxxB0g7WLEOrpl92TpCNtsrs2Y=;EntityPath=topica";
+        //private static string queue_name = "niks";
+        private static string topic_name = "topica";
+        private static string Subscription_name = "SubscriptionA";
+
+        static async Task Main(string[] args)
+        {
+            ServiceBusClient _client = new ServiceBusClient(connection_string);
+            ServiceBusReceiver _receiver = _client.CreateReceiver(topic_name,Subscription_name, new ServiceBusReceiverOptions() { ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete });
+
+            //ServiceBusReceiver _receiver = _client.CreateReceiver(queue_name, new ServiceBusReceiverOptions() { ReceiveMode = ServiceBusReceiveMode.PeekLock });
+
+            var _messgaes = _receiver.ReceiveMessagesAsync(200000);
+
+            foreach (var _message in _messgaes.Result)
+            {
+                Console.WriteLine($"The Sequence number is {_message.SequenceNumber}");
+                Console.WriteLine(_message.Body);
+            }
+        }
+    }
+}
--- ServiceBus/SendMessage/Program.cs
+++ ServiceBus/SendMessage/Program.cs
@@ -6,28 +6,43 @@ namespace AzureServiceQueue
 {
     class Program
     {
-        private static string connection_string = "Endpoint=sb://niksservice.servicebus.windows.net/;SharedAccessKeyName=Send;SharedAccessKey=XjRhcW0Z09svEbzsAsB7ECwUGyU+4GfpNQQua3/mg/M=;";
-        private static string queue_name = "niksservice";
+        private static string connection_string = "Endpoint=sb://niksservice.servicebus.windows.net/;SharedAccessKeyName=Send;SharedAccessKey=2Id1BEm3jMCSqMt15L1UITnYzXdTyWbB+1sLiXhYH6M=;EntityPath=topica";
+        //private static string queue_name = "niks";
+        private static string topic_name = "topica";
+
 
         static void Main(string[] args)
         {
             List<Order> _orders = new List<Order>()
             {
-                new Order() {OrderID="O1",Quantity=10,UnitPrice=9.99m},
-                new Order() {OrderID="O2",Quantity=15,UnitPrice=10.99m },
-                new Order() {OrderID="O3",Quantity=20,UnitPrice=11.99m},
-                new Order() {OrderID="O4",Quantity=25,UnitPrice=12.99m},
-                new Order() {OrderID="O5",Quantity=30,UnitPrice=13.99m }
+                new Order() {OrderID="1",Quantity=10,UnitPrice=9.99m},
+                new Order() {OrderID="2",Quantity=15,UnitPrice=10.99m },
+                new Order() {OrderID="3",Quantity=20,UnitPrice=11.99m},
+                new Order() {OrderID="4",Quantity=25,UnitPrice=12.99m},
+                new Order() {OrderID="5",Quantity=30,UnitPrice=13.99m }
             };
 
             ServiceBusClient _client = new ServiceBusClient(connection_string);
-            ServiceBusSender _sender = _client.CreateSender(queue_name);
+            //ServiceBusSender _sender = _client.CreateSender(queue_name);
+            ServiceBusSender _sender = _client.CreateSender(topic_name);
 
             foreach (Order _order in _orders)
             {
                 ServiceBusMessage _message = new ServiceBusMessage(_order.ToString());
                 _message.ContentType = "application/json";
+
+                if (Convert.ToInt32(_order.OrderID) > 3)
+                {
+                   _message.ApplicationProperties.Add("Subscription", "B");
+                }
+                else
+                {
+                    _message.ApplicationProperties.Add("Subscription", "A");
+                }
+                
                 _sender.SendMessageAsync(_message).GetAwaiter().GetResult();
+
+                Console.WriteLine(_message.Body + "Sent");
             }
         }
     }
